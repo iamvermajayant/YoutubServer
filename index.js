ef58d3e85c38bb0e946636d86277b94d8647c89e
Jayant Verma <iamvermajayant@gmail.com>
2023-09-20 19:13:17 +0530

    added mongo db connection

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
+const connect = () => {
+    mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("Connected to DB");
+    })
+    .catch((error)=>{
+        throw error;
+    })
+}
 
 app.get('/', (req,res)=> {
     res.send("helllo Worlds");
-})
-
+});
 
 app.listen(3000, ()=>{
+
+    connect();
     console.log(`running on 3000`);
 })
