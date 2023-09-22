7222bbdc8be16732ce7ca72f24149b8de000a577
Jayant Verma <iamvermajayant@gmail.com>
2023-09-22 19:33:20 +0530

    created the user API endpoint

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,17 @@
 import mongoose from "mongoose"
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
 
 export const signup = async (req,res) => {
     try {
-        console.log(req.body);
-        // const newUser = new User()
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(req.body.password, salt);
+
+        const newUser = new User({...req.body, password : hash});
+
+        await newUser.save();
+        res.status(200).send("user has been created");
     } catch (error) { 
-        
+        res.send("Error" +  error);
     }
 }
