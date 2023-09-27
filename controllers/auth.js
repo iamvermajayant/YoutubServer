f702ca6659dd0587ef9f8c037036e21f61ad0c4c
Jayant Verma <iamvermajayant@gmail.com>
2023-09-27 19:31:16 +0530

    updated user Controller function done

--- controllers/auth.js
+++ controllers/auth.js
@@ -32,7 +32,7 @@ export const signIn = async (req, res, next) => {
        const token = jwt.sign({id : user._id}, process.env.JWT);
        const {password , ...others} = user._doc;
 
-       res.cookie("access token", token, {
+       res.cookie("access_token", token, {
         httpOnly: true,
        }).status(200)
        .json(others);
