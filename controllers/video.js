7c244ad80761f61ee2521143a7d0cb005ee4f332
Jayant Verma <iamvermajayant@gmail.com>
2023-09-28 19:30:22 +0530

    add Video controller function Done

--- controllers/video.js
+++ controllers/video.js
@@ -1,7 +1,10 @@
 import Video from '../models/Video.js'
 
 export const addVideo = async (req, res, next) => {
+    const newVideo = new Video({UserId : req.user.id, ...req.user.body})
+    const savedVideo = await newVideo.save();
 
+    res.status(200).json(savedVideo);
 }
 
 
