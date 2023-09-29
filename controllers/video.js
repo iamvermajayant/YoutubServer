fc3c52031ae778e4aad8acafea493ff37dffa89f
Jayant Verma <iamvermajayant@gmail.com>
2023-09-29 13:20:00 +0530

    updated video function done

--- controllers/video.js
+++ controllers/video.js
@@ -1,4 +1,6 @@
 import Video from '../models/Video.js'
+import { createError } from '../error.js';
+
 
 export const addVideo = async (req, res, next) => {
     try {
@@ -14,7 +16,19 @@ export const addVideo = async (req, res, next) => {
 
 
 export const updateVideo = async (req, res, next) => {
+     const video = await Video.findById(req.params.id);
+     if(!video) return next(createError(404, "Video not found"));
 
+     if(req.user.id === video.UserId){
+        const updatedUser = await Video.findByIdAndUpdate(req.params.id, {
+            $set : req.body
+        }, 
+        {
+            new : true
+        })
+
+        res.status(200).json(updateVideo);
+     }
 }
 
 
